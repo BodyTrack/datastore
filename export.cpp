084acee246d6ac671602c7b242617e1ee4cb1595
Chris Bartley <bartley@cmu.edu>
2015-06-10 17:49:21 -0400

    Initial support for JSON export

--- export.cpp
+++ export.cpp
@@ -22,8 +22,7 @@
 
 int double_precision_digits = 15;
 
-void usage(const char *fmt, ...)
-{
+void usage(const char *fmt, ...) {
   std::cerr << "\n";
   va_list args;
   va_start(args, fmt);
@@ -31,13 +30,14 @@ void usage(const char *fmt, ...)
   va_end(args);
   std::cerr << "\n";
   std::cerr << "\n";
-  
+
   std::cerr << "Usage:\n";
   std::cerr << "export [flags] store.kvs uid dev_nickname.ch_name [dev_nickname.ch_name ...]\n";
   std::cerr << "export [flags] store.kvs uid.dev_nickname.ch_name [uid.dev_nickname.ch_name ...]\n";
   std::cerr << "   --start:  start time (floating-point epoch time).  Defaults to beginning of time.\n";
   std::cerr << "   --end:    end time (floating-point epoch time).  Defaults to end of time.\n";
   std::cerr << "   --csv:    export in CSV format\n";
+  std::cerr << "   --json:   export in JSON format\n";
   std::cerr << "   --full-precision:  export full-precision data\n";
   std::cerr << "             IEEE 754 double-precision values have ~15.6 digits of precision.\n";
   std::cerr << "             Default output uses 15 digits of precision to avoid displaying\n";
@@ -49,8 +49,7 @@ void usage(const char *fmt, ...)
   exit(1);
 }
 
-bool dump_samples(const Tile &tile, Range requested_times)
-{
+bool dump_samples(const Tile &tile, Range requested_times) {
   unsigned double_index = 0, string_index = 0;
 
   while (1) {
@@ -69,15 +68,15 @@ bool dump_samples(const Tile &tile, Range requested_times)
       // Select the earliest sample from double or string
       select_double = tile.double_samples[double_index].time <= tile.string_samples[string_index].time;
     }
-    
+
     if (select_double) {
       DataSample<double> sample = tile.double_samples[double_index++];
-      if (requested_times.includes(sample.time)) 
-	printf("%.*g\t%*g\n", double_precision_digits, sample.time, double_precision_digits, sample.value);
+      if (requested_times.includes(sample.time))
+        printf("%.*g\t%*g\n", double_precision_digits, sample.time, double_precision_digits, sample.value);
     } else {
       DataSample<std::string> sample = tile.string_samples[string_index++];
       if (requested_times.includes(sample.time))
-	printf("%.*g\t%s\n", double_precision_digits, sample.time, sample.value.c_str());
+        printf("%.*g\t%s\n", double_precision_digits, sample.time, sample.value.c_str());
     }
   }
   return true;
@@ -125,13 +124,13 @@ private:
   Tile tile;
   unsigned double_index, string_index;
   int desired_level;
-  
+
 public:
-  ChannelReader(const std::string &channel_name, simple_shared_ptr<Channel> channel, double time) 
+  ChannelReader(const std::string &channel_name, simple_shared_ptr<Channel> channel, double time)
     : channel_name(channel_name), channel(channel), desired_level(TileIndex::lowest_level()) {
     seek(time);
   }
-  
+
   // Seek to first time >= new_time
   void seek(double new_time) {
     TileIndex root = root_tile();
@@ -142,6 +141,7 @@ public:
       while (time() < new_time) advance();
     }
   }
+
   // Returns NULL if no double_sample at current time, or if no more samples available
   DataSample<double> *double_sample() {
     if (!ti.is_null() && double_index < tile.double_samples.size()) {
@@ -157,7 +157,7 @@ public:
     }
     return NULL;
   }
-  
+
   // Returns DBL_MAX when no more samples available
   double time() {
     double t = DBL_MAX;
@@ -165,13 +165,13 @@ public:
     if (string_sample()) t = std::min(t, string_sample()->time);
     return t;
   }
-  
+
   // Advance to next available timestamp
   void advance() {
     if (ti.is_null()) {
       return;
     }
-    
+
     double t = time();
     if (double_sample() && double_sample()->time == t) double_index++;
     if (string_sample() && string_sample()->time == t) string_index++;
@@ -185,7 +185,7 @@ public:
       }
     }
   }
-  
+
 private:
   void read_tile_or_successor(TileIndex tile_index, TileIndex root) {
     while (1) {
@@ -193,15 +193,15 @@ private:
       ti = tile_index;
       double_index = string_index = 0;
       if (!ti.is_null()) {
-	if (!channel->read_tile(ti, tile)) {
-	  ti = TileIndex::null();
-	} else {
-	  if (!double_sample() && !string_sample()) {
-	    // Empty tile?  skip to next
-	    tile_index = channel->find_successive_tile(root, tile_index, desired_level);
-	    continue;
-	  }
-	}
+        if (!channel->read_tile(ti, tile)) {
+          ti = TileIndex::null();
+        } else {
+          if (!double_sample() && !string_sample()) {
+            // Empty tile?  skip to next
+            tile_index = channel->find_successive_tile(root, tile_index, desired_level);
+            continue;
+          }
+        }
       }
       return;
     }
@@ -226,9 +226,9 @@ void export_csv(KVS &store, Range timerange, int uid, const std::vector<std::str
       ch.reset(new Channel(store, uid, channel_full_names[i]));
     }
     readers.push_back(simple_shared_ptr<ChannelReader>
-                      (new ChannelReader(channel_full_names[i], ch, timerange.min)));
+                          (new ChannelReader(channel_full_names[i], ch, timerange.min)));
   }
-  
+
   // Emit header
   std::cout << quote_csv("EpochTime");
   for (unsigned i = 0; i < readers.size(); i++) {
@@ -271,15 +271,85 @@ void export_csv(KVS &store, Range timerange, int uid, const std::vector<std::str
   }
 }
 
-int execute(Arglist args)
-{
+void export_json(KVS &store, Range timerange, int uid, const std::vector<std::string> &channel_full_names) {
+  std::vector<simple_shared_ptr<ChannelReader> > readers;
+
+  for (unsigned i = 0; i < channel_full_names.size(); i++) {
+    simple_shared_ptr<Channel> ch;
+    if (uid == -1) {
+      ch.reset(new Channel(store, channel_full_names[i]));
+    } else {
+      ch.reset(new Channel(store, uid, channel_full_names[i]));
+    }
+    readers.push_back(simple_shared_ptr<ChannelReader>
+                        (new ChannelReader(channel_full_names[i], ch, timerange.min)));
+  }
+
+  // Emit header
+  std::cout << "{\"channel_names\":[";
+  for (unsigned i = 0; i < readers.size(); i++) {
+    std::cout << "\"" << quote_csv(channel_full_names[i]) << "\"";
+    if (i < readers.size() - 1) {
+      std::cout << ",";
+    }
+  }
+  std::cout << "],";
+
+  // emit the data
+  std::cout << "\"data\":[";
+
+  bool hasPrintedAtLeastOneLine = false;
+  while (1) {
+    // Find next time -- min of all channels' next values
+    double time = DBL_MAX;
+    for (unsigned i = 0; i < readers.size(); i++) {
+      time = std::min(time, readers[i]->time());
+    }
+
+    // If no more samples in range, we're done
+    if (time > timerange.max || time == DBL_MAX) break;
+
+    // Emit any samples that match this time.  Emit empty entries
+    // for channels that don't have a for this time
+
+    if (hasPrintedAtLeastOneLine) {
+      std::cout << ",";
+    }
+    printf("\n[%.*g", double_precision_digits, time);
+    for (unsigned i = 0; i < readers.size(); i++) {
+      std::cout << ",";
+      if (readers[i]->time() == time) {
+        // If channel has a double and string sample at same time, just output the double
+        if (readers[i]->double_sample()) {
+          printf("%.*g", double_precision_digits, readers[i]->double_sample()->value);
+        } else if (readers[i]->string_sample()) {
+          std::cout << quote_csv(readers[i]->string_sample()->value).c_str();
+        } else {
+          abort();
+        }
+        readers[i]->advance();
+      } else {
+        // No sample at this time;  empty column
+        std::cout << "null";
+      }
+    }
+    std::cout << "]";
+    hasPrintedAtLeastOneLine = true;
+  }
+
+  // close it off
+  std::cout << "\n]}\n";
+}
+
+int execute(Arglist args) {
   std::string invocation = args.to_string();
   enum {
     LEGACY_FORMAT = 1,
-    CSV_FORMAT
+    CSV_FORMAT,
+    JSON_FORMAT
   } format = LEGACY_FORMAT;
   Range timerange = Range::all();
-  
+
   std::string storename;
   int uid = -1;
   std::vector<std::string> channel_full_names;
@@ -288,6 +358,8 @@ int execute(Arglist args)
     std::string arg = args.shift();
     if (arg == "--csv") {
       format = CSV_FORMAT;
+    } else if (arg == "--json") {
+      format = JSON_FORMAT;
     } else if (arg == "--start") {
       timerange.min = args.shift_double();
     } else if (arg == "--end") {
@@ -312,29 +384,31 @@ int execute(Arglist args)
 
   if (storename == "") usage("Missing store");
   if (channel_full_names.size() == 0) usage("No channels specified");
-  
+
   set_log_prefix(string_printf("%d %d ", getpid(), uid));
-  
+
   log_f("export START: %s", invocation.c_str());
-  
+
   FilesystemKVS store(storename.c_str());
   //store.set_verbosity(100);
-  
+
   switch (format) {
-  case LEGACY_FORMAT:
-    export_legacy(store, timerange, uid, channel_full_names);
-    break;
-  case CSV_FORMAT:
-    export_csv(store, timerange, uid, channel_full_names);
-    break;
-  default:
-    assert(0);
+    case LEGACY_FORMAT:
+      export_legacy(store, timerange, uid, channel_full_names);
+      break;
+    case CSV_FORMAT:
+      export_csv(store, timerange, uid, channel_full_names);
+      break;
+    case JSON_FORMAT:
+      export_json(store, timerange, uid, channel_full_names);
+      break;
+    default:
+      assert(0);
   }
   return 0;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   int exit_code = 1;
   try {
     exit_code = execute(Arglist(argv + 1, argv + argc));
